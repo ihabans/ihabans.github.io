0f3f1a538ff1ac32185e6080328e525b13a2f0ea
ihabans <ihab.anseur@efrei.net>
2021-12-31 17:52:57 +0100

    Ihab and Sabri na Website

---
+++ script.semaine.js
@@ -0,0 +1,13 @@
+
+var selectForm = document.forms[index];
+var selectFormElement = document.forms[index].elements[index];
+
+
+function add(){
+   alert("hello")
+   console.log(Questionnaire.elements.nom.value);
+    console.log(Questionnaire.elements.prenom.value);
+}
+
+
+if(isset())
